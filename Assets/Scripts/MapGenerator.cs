4da91aa2290c7b20970833f9db5ef5bb892bdf87
Jukka Eerikäinen <jukkae@iki.fi>
2019-12-07 18:42:03 +0200

    move map generation to, well, MapGenerator

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,6 +7,22 @@ public static class MapGenerator
     public static Tile[,] GenerateMap(int width, int height)
     {
         Tile[,] tiles = new Tile[width, height];
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (i == 0 || i == width - 1 || j == 0 || j == height - 1)
+                {
+                    tiles[i, j] = new Tile(TileType.Wall);
+                }
+                else
+                {
+                    tiles[i, j] = new Tile(TileType.Floor);
+                }
+            }
+        }
+
         return tiles;
     }
 }
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -28,30 +28,36 @@ public class World : MonoBehaviour
 
     void Start()
     {
-        MapGenerator.GenerateMap(width, height);
         map = new GameObject
         {
             name = "Map"
         };
-
         map.transform.parent = transform;
-        tiles = new Tile[width, height];
+
+        tiles = MapGenerator.GenerateMap(width, height);
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                Tile tile = tiles[i, j];
+                if (tile.type == TileType.Wall)
+                {
+                    var g = Instantiate(wallTile, new Vector3(i, 0, j), Quaternion.identity);
+                    g.transform.parent = map.transform;
+                }
+                else if (tile.type == TileType.Floor)
+                {
+                    var g = Instantiate(floorTile, new Vector3(i, 0, j), Quaternion.identity);
+                    g.transform.parent = map.transform;
+                }
+            }
+        }
+
+        // Let there be light
         for (int i = 0; i < width; i++)
         {
             for(int j = 0; j < height; j++)
             {
-                if(i == 0 || i == width - 1 || j == 0 || j == height - 1)
-                {
-                    tiles[i, j] = new Tile(TileType.Wall);
-                    var g = Instantiate(wallTile, new Vector3(i, 0, j), Quaternion.identity);
-                    g.transform.parent = map.transform;
-                }
-                else
-                {
-                    tiles[i, j] = new Tile(TileType.Floor);
-                    var g = Instantiate(floorTile, new Vector3(i, 0, j), Quaternion.identity);
-                    g.transform.parent = map.transform;
-                }
                 if(i % 10 == 0 && j % 10 == 0)
                 {
                     var g = Instantiate(pointLight, new Vector3(i, 3, j), Quaternion.identity);
